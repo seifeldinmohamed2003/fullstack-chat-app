b0b83d2cfe54a7162f0a992d26f7d027e8f415fc
Seif el din Mohamed <seifeldinmohamed79@gmail.com>
2025-08-09 19:07:28 +0300

    Created the schema for the user

---
+++ backend/models/usermodel.js
@@ -0,0 +1,27 @@
+const mongoose = require('mongoose');
+const { Schema } = mongoose;
+
+const userSchema = new mongoose.Schema({
+    username: {
+        type: String,
+        required: true,
+        unique: true,
+    },
+    email: {
+        type: String,
+        required: true,
+        unique: true,
+    },
+    password: {
+        type: String,
+        required: true,
+    },
+    avatar: {
+        type: String,
+    },
+    isOnline: {
+        type: Boolean,
+        default: false,
+    },
+
+})
