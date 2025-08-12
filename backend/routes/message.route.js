b0b22659d6b786973eb1704ba0e2d9f99a705624
Seif el din Mohamed <seifeldinmohamed79@gmail.com>
2025-08-12 21:49:40 +0300

    Started working on the front end by creating boiler plate code for the pages

--- backend/routes/message.route.js
+++ backend/routes/message.route.js
@@ -1,5 +1,5 @@
 import express from 'express';
-import { protectRoute } from '../middleware/auth.middleware';
+import { protectRoute } from '../middleware/auth.middleware.js';
 import { getUsersForSidebar,getMessages,sendMessage } from '../controllers/message.controller.js';
 
 
